38edc4f4098253c866c7f0a2566a4e094abd29a2
Dong-Han-Kim <dh32110@gmail.com>
2024-06-19 22:47:51 +0900

    검색어 굵은 글씨로 만들기

---
+++ src/App.jsx
@@ -0,0 +1,80 @@
+import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { useState } from 'react';
+import { search } from './assets/searchWord';
+import { text } from '@fortawesome/fontawesome-svg-core';
+
+function App() {
+	const [value, setValue] = useState('');
+	const [result, setResult] = useState([]);
+	const [searched, setSearched] = useState(false);
+
+	const handleInputChange = (e) => {
+		const inputValue = e.target.value;
+		setValue(inputValue);
+		setResult([]);
+		setSearched(false);
+	};
+
+	function findValue() {
+		const inputValue = value.toLowerCase();
+		const searchFilter = search.filter((word) => word.sentence.toLowerCase().includes(inputValue));
+		setResult(searchFilter);
+		setSearched(true);
+	}
+
+	function findStrong(resultItem, strongValue) {
+		if (!strongValue) return text;
+		const lowResultItem = resultItem.toLowerCase();
+		const lowStrongValue = strongValue.toLowerCase();
+		const startIndex = lowResultItem.indexOf(lowStrongValue);
+
+		if (startIndex === -1) {
+			return text;
+		}
+
+		const endIndex = startIndex + strongValue.length;
+		const front = resultItem.slice(0, startIndex);
+		const strongWords = resultItem.slice(startIndex, endIndex);
+		const back = resultItem.slice(endIndex);
+		return (
+			<span>
+				{front}
+				<strong>{strongWords}</strong>
+				{back}
+			</span>
+		);
+	}
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		findValue();
+	};
+
+	return (
+		<main className="main">
+			<section className="search">
+				<form onSubmit={handleSubmit}>
+					<input type="text" value={value} onChange={handleInputChange} />
+					<button onClick={findValue}>
+						<FontAwesomeIcon icon={faSearch} />
+					</button>
+				</form>
+			</section>
+
+			<section className="result">
+				{result.length > 0 && (
+					<ul className="result_list">
+						{result.map((item) => {
+							return <li key={item.id}>{findStrong(item.sentence, value)}</li>;
+						})}
+					</ul>
+				)}
+				{searched && result.length === 0 && '검색 결과가 없습니다.'}
+			</section>
+		</main>
+	);
+}
+
+export default App;
---
+++ src/assets/searchWord.js
@@ -0,0 +1,34 @@
+export const search = [
+	{
+		id: 'a',
+		sentence: 'a 태그가 있습니다.',
+	},
+	{
+		id: 'b',
+		sentence: 'b 태그가 있습니다.',
+	},
+	{
+		id: 'div',
+		sentence: 'div 태그가 있습니다.',
+	},
+	{
+		id: 'link',
+		sentence: 'link 태그가 있습니다.',
+	},
+	{
+		id: 'main',
+		sentence: 'main 태그가 있습니다.',
+	},
+	{
+		id: 'head',
+		sentence: 'head 태그가 있습니다.',
+	},
+	{
+		id: 'footer',
+		sentence: 'footer 태그가 있습니다.',
+	},
+	{
+		id: 'span',
+		sentence: 'span 태그가 있습니다.',
+	},
+];
