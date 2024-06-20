2ba3625d1d924062bbaa70ca0aa5e98db3f25a6b
Dong-Han-Kim <dh32110@gmail.com>
2024-06-20 16:39:00 +0900

    wip

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,22 @@ function App() {
 	const [value, setValue] = useState('');
 	const [result, setResult] = useState([]);
 	const [searched, setSearched] = useState(false);
+	const [autoComplete, setAuotoComplete] = useState([]);
 
 	const handleInputChange = (e) => {
 		const inputValue = e.target.value;
 		setValue(inputValue);
 		setResult([]);
 		setSearched(false);
+		fliterAutoComplete(inputValue);
+	};
+
+	const fliterAutoComplete = (inputValue) => {
+		if (!inputValue) return setAuotoComplete([]);
+		const fliterValue = search
+			.filter((item) => item.sentence.toLowerCase().includes(inputValue))
+			.map((item) => item.sentence);
+		setAuotoComplete(fliterValue);
 	};
 
 	function findValue() {
@@ -21,16 +31,17 @@ function App() {
 		const searchFilter = search.filter((word) => word.sentence.toLowerCase().includes(inputValue));
 		setResult(searchFilter);
 		setSearched(true);
+		setAuotoComplete([]);
 	}
 
 	function findStrong(resultItem, strongValue) {
-		if (!strongValue) return resultItem;
+		if (!strongValue) return null;
 		const lowResultItem = resultItem.toLowerCase();
 		const lowStrongValue = strongValue.toLowerCase();
 		const startIndex = lowResultItem.indexOf(lowStrongValue);
 
 		if (startIndex === -1) {
-			return resultItem;
+			return null;
 		}
 
 		const endIndex = startIndex + strongValue.length;
@@ -53,25 +64,37 @@ function App() {
 
 	return (
 		<main className="main">
-			<section className="search">
-				<form onSubmit={handleSubmit}>
-					<input type="text" value={value} onChange={handleInputChange} />
-					<button onClick={findValue}>
-						<FontAwesomeIcon icon={faSearch} />
-					</button>
-				</form>
-			</section>
+			<div className="main_content">
+				<section className="search">
+					<form onSubmit={handleSubmit}>
+						<input type="text" value={value} onChange={handleInputChange} />
+						<button onClick={findValue}>
+							<FontAwesomeIcon icon={faSearch} />
+						</button>
+					</form>
+					{autoComplete.length > 0 && (
+						<div className="auto_complete">
+							<p className="auto_description">검색 가능한 추천 목록</p>
+							<ul className="result_list">
+								{autoComplete.map((item, i) => {
+									return <li key={i}>{findStrong(item, value)}</li>;
+								})}
+							</ul>
+						</div>
+					)}
+				</section>
 
-			<section className="result">
-				{result.length > 0 && (
-					<ul className="result_list">
-						{result.map((item) => {
-							return <li key={item.id}>{findStrong(item.sentence, value)}</li>;
-						})}
-					</ul>
-				)}
-				{searched && result.length === 0 && '검색 결과가 없습니다. 검색어와 띄어쓰기를 확인해주세요.'}
-			</section>
+				<section className="result">
+					{result.length > 0 && (
+						<ul className="result_list">
+							{result.map((item) => {
+								return <li key={item.id}>{findStrong(item.sentence, value)}</li>;
+							})}
+						</ul>
+					)}
+					{searched && result.length === 0 && '검색 결과가 없습니다. 검색어와 띄어쓰기를 확인해주세요.'}
+				</section>
+			</div>
 		</main>
 	);
 }
